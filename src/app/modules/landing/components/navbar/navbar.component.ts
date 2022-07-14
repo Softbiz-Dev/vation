6302cfe4cda861827497f65e5cc73a6f5d37aafd
carlosstack <paytocarlos@gmail.com>
2022-07-14 12:36:51 -0400

    avances

--- src/app/modules/landing/components/navbar/navbar.component.ts
+++ src/app/modules/landing/components/navbar/navbar.component.ts
@@ -19,7 +19,7 @@ export class NavbarComponent implements OnInit {
       || document.body.scrollTop || 0;
 
     if (verticalOffset > 100) {
-      document.getElementById("navbar")!!.style.backgroundColor = "#121f33"
+      document.getElementById("navbar")!!.style.backgroundColor = "#121f33e0"
     } else {
       document.getElementById("navbar")!!.style.backgroundColor = "transparent"
     }
