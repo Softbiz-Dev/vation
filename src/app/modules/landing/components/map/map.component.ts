0c47e374a119acb082a7eeebb6591d5ebff35c66
Carlos Hernández <carlos.hernandez@correo.unimet.edu.ve>
2022-07-21 00:04:02 -0300

    incorporando animaciones

--- src/app/modules/landing/components/map/map.component.ts
+++ src/app/modules/landing/components/map/map.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import DottedMap from 'dotted-map';
+// Or in the browser: import DottedMap from 'dotted-map';
+
 
 @Component({
   selector: 'app-map',
@@ -7,18 +11,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapComponent implements OnInit {
 
-  constructor() { }
+  map:any;
+
+  constructor(private sanitization:DomSanitizer) {
+
+  }
 
   ngOnInit(): void {
 
+    //this.get()
 
-    var img = document.getElementById('img');
+    /* var img = document.getElementById('img');
     var coords = document.getElementById('coords');
     img!!.addEventListener('mousemove', function (event) {
 
       coords!!.innerHTML = "x: " + event.offsetX + "<br/>y: " + event.offsetY;
+    }); */
+
+  }
+
+  get(){
+    const map = new DottedMap({ height: 80, grid: 'diagonal', countries:["VEN"] });
+
+    map.addPin({
+      lat: 10.5,
+      lng: -66.916664,
+      svgOptions: { color: '#fff', radius: 0.4 },
+    });
+    map.addPin({
+      lat: 48.8534,
+      lng: 2.3488,
+      svgOptions: { color: '#fff', radius: 0.4 },
     });
 
+    const svgMap = map.getSVG({
+      radius: 0.22,
+      color: '#423B38',
+      shape: 'circle',
+      backgroundColor: 'transparent',
+    });
+
+    //onsole.log(svgMap);
+
+
+    this.map = this.sanitization.bypassSecurityTrustHtml(svgMap)
   }
 
 }
